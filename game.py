5893ec754c6d39de62edcc7088e2ac1a36f948d4
Pedram Allahyari <pedram.allahyari.1387@gmail.com>
2026-09-24 03:36:03 +0330

    Add W A S D controls

--- game.py
+++ game.py
@@ -13,4 +13,32 @@ leaf = Label(win, text=direction)
 leaf.place(x=200, y=170)
 leaf.config(font=("Arial", 30))
 
+def up():
+    print("up")
+
+def left():
+    print("left")
+
+def right():
+    print("right")
+
+def down():
+    print("down")
+
+bt1 = Button(win, text="W", command=up)
+bt1.place(x=230, y=300)
+bt1.config(width=5)
+
+bt2 = Button(win, text="A", command=left)
+bt2.place(x=180, y=340)
+bt2.config(width=5)
+
+bt3 = Button(win, text="S", command=down)
+bt3.place(x=230, y=340)
+bt3.config(width=5)
+
+bt4 = Button(win, text="D", command=right)
+bt4.place(x=280, y=340)
+bt4.config(width=5)
+
 win.mainloop()
