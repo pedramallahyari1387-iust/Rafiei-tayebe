735cc5b245f1c7fd5c80f3179f25fd87b504a57e
Pedram Allahyari <pedram.allahyari.1387@gmail.com>
2026-09-24 03:33:42 +0330

    Add random leaf direction

--- game.py
+++ game.py
@@ -1,8 +1,16 @@
 from tkinter import *
+import random
 
 win = Tk()
 win.title("Ebb and Flow")
 win.geometry("500x500")
 win.resizable(width=False, height=False)
+
+directions = [">>>>>", "<<<<<", "^^^^^", "vvvvv"]
+direction = random.choice(directions)
+
+leaf = Label(win, text=direction)
+leaf.place(x=200, y=170)
+leaf.config(font=("Arial", 30))
 
 win.mainloop()
